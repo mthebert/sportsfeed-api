4519e6f58cbf6d4fc9bbebf6043ef3ccf3dd5a70
mthebert <matt@thebert.com>
2019-05-29 22:39:04 -0400

    add redis cache to get routes

--- routes/hockeyRoutes.js
+++ routes/hockeyRoutes.js
@@ -1,7 +1,8 @@
 const controller = require('../controllers/hockeyController');
+const cache = require('express-redis-cache')();
 
 module.exports = function(app) {
-  app.route('/hockey/teams/season/stats').get(controller.get_all_season_stats);
-  app.route('/hockey/teams/playoff/stats').get(controller.get_all_playoff_stats);
-  app.route('/hockey/teams').get(controller.get_teams);
+  app.route('/hockey/teams/season/stats').get(cache.route(), controller.get_all_season_stats);
+  app.route('/hockey/teams/playoff/stats').get(cache.route(), controller.get_all_playoff_stats);
+  app.route('/hockey/teams').get(cache.route(), controller.get_teams);
 };
